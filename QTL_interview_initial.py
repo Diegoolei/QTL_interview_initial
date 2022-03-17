e7ee1b5b2a6cf33437940055382d4e275f4c6c8c
Diegoolei <Diegoolei@github.com>
2022-03-17 00:58:27 -0300

    Asset generalization

--- QTL_interview_initial.py
+++ QTL_interview_initial.py
@@ -12,14 +12,34 @@ class crypto_info:
         self.ohlc       = None
 
 
-    def exchange_ticker (self):
-        self.ticker     = json.dumps(self.exchange().fetch_ticker('KDA/USDT'), indent=2)
+    def exchange_ticker (self, crypto_a: str, crypto_b: str):
+        if (crypto_a + "/" + crypto_b) in self.market:
+            self.ticker     = json.dumps(self.exchange().fetch_ticker(crypto_a + "/" + crypto_b), indent=2)
 
+
+    # Lists all available exchanges in ccxt
+    def list_exchanges():
+        for exchange in ccxt.exchanges: 
+            print(exchange)
+
+
+    # Available Currency trades in current exchange AND ITS INFO
+    def update_exchange_market (self):
+        self.market     = self.exchange().load_markets()
+
+
+    # # Open-high-low-close values
+    # def update_exchange_ohlc (self, crypto_a: str, crypto_b: str):
+    #     if (crypto_a + "/" + crypto_b) in self.market:
+    #         self.ohlc       = json.dumps(self.exchange().fetch_ohlcv(crypto_a + "/" + crypto_b), indent=2) 
 
 
 binance = crypto_info("binance")
-print(binance.ticker) # Should be None
 
-binance.exchange_ticker()
+binance.update_exchange_market()
+
+binance.exchange_ticker("ETH", "BTC")
 print(binance.ticker) # Should be full ticker
-        
+
+# binance.update_exchange_ohlc("ETH", "BTC")
+# print(binance.ohlc)
