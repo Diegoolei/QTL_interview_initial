21340d2e6b37f90bc1acb24cbe8d9f1c43e0e45a
Diegoolei <Diegoolei@github.com>
2022-03-17 11:05:20 -0300

    Added crypto class

--- QTL_interview_initial.py
+++ QTL_interview_initial.py
@@ -2,44 +2,57 @@ import json
 import ccxt
 
 
-class crypto_info:
+class exchange_info:
     
-    # Initializes crypto object to be interacted with
+    # Initializes market object to be interacted with
     def __init__(self, exchange_id : str) -> None:
         self.exchange   = getattr(ccxt, exchange_id)
-        self.market     = None
-        self.ticker     = None
-        self.ohlc       = None
+        self.market     = self.exchange().load_markets()
+        self.crypto_a   = None
+        self.crypto_b   = None
 
-
-    def exchange_ticker (self, crypto_a: str, crypto_b: str):
-        if (crypto_a + "/" + crypto_b) in self.market:
-            self.ticker     = json.dumps(self.exchange().fetch_ticker(crypto_a + "/" + crypto_b), indent=2)
+    def crypto_check(self):
+        return (self.crypto_a and self.crypto_b)
 
 
     # Lists all available exchanges in ccxt
-    def list_exchanges():
+    def list_exchanges(self):
         for exchange in ccxt.exchanges: 
             print(exchange)
+
+    def update_exchange(self, exchange_id : str) -> None:
+        self.exchange   = getattr(ccxt, exchange_id)
+        self.market     = self.exchange().load_markets()
+        self.crypto_a_b   = None
+        self.crypto_b_a   = None
 
 
     # Available Currency trades in current exchange AND ITS INFO
     def update_exchange_market (self):
-        self.market     = self.exchange().load_markets()
+        self.market = self.exchange().load_markets()
 
 
-    # # Open-high-low-close values
-    # def update_exchange_ohlc (self, crypto_a: str, crypto_b: str):
-    #     if (crypto_a + "/" + crypto_b) in self.market:
-    #         self.ohlc       = json.dumps(self.exchange().fetch_ohlcv(crypto_a + "/" + crypto_b), indent=2) 
+
+class crypto_info:
+    def __init__(self, exchange: exchange_info, crypto_a : str, crypto_b : str) -> None:
+        self.market = exchange.market
+        self.crypto_a: str = crypto_a   #Crypto shorted names
+        self.crypto_b: str = crypto_b   #
+        self.valid_market  = False      #Valid only if market extists in exchange
+        self.ticker     = None
+
+    def crypto_ticker (self) -> None:
+        if (self.ticker_a + "/" + self.ticker_b) in self.market:
+            self.ticker = json.dumps(self.exchange().fetch_ticker(
+                                        self.crypto_a + "/" + self.crypto_b), indent=4
+                                        )
+        else:
+            self.ticker = None
 
 
-binance = crypto_info("binance")
+binance = exchange_info("binance")
 
 binance.update_exchange_market()
 
-binance.exchange_ticker("ETH", "BTC")
+binance.exchange_ticker()
 print(binance.ticker) # Should be full ticker
-
-# binance.update_exchange_ohlc("ETH", "BTC")
-# print(binance.ohlc)
